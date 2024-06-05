c2769a0e9bc26edc5f8a14ec714578c041fd26d3
Rui Chen <rui@chenrui.dev>
2024-06-05 08:14:36 -0400

    inframap 0.7.0
    inframap: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    autobump: add inframap
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/inframap.rb
+++ Formula/i/inframap.rb
@@ -1,8 +1,8 @@
 class Inframap < Formula
   desc "Read your tfstate or HCL to generate a graph"
   homepage "https://github.com/cycloidio/inframap"
-  url "https://github.com/cycloidio/inframap/archive/refs/tags/v0.6.7.tar.gz"
-  sha256 "e9d6daa48c6fa1a8ecc5437c7121cb5072eb81c29c88ca9e6d778637c8442332"
+  url "https://github.com/cycloidio/inframap/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "de13ab29e5db0a7a03849b0464f54cf3c6b63bce76a8b257974ffee267f033e9"
   license "MIT"
   head "https://github.com/cycloidio/inframap.git", branch: "master"
 
@@ -22,18 +22,18 @@ class Inframap < Formula
 
   depends_on "go" => :build
 
-  resource "test_resource" do
-    url "https://raw.githubusercontent.com/cycloidio/inframap/7ef22e7/generate/testdata/azure.tfstate"
-    sha256 "633033074a8ac43df3d0ef0881f14abd47a850b4afd5f1fbe02d3885b8e8104d"
-  end
-
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/cycloidio/inframap/cmd.Version=v#{version}")
   end
 
   test do
+    resource "homebrew-test_resource" do
+      url "https://raw.githubusercontent.com/cycloidio/inframap/7ef22e7/generate/testdata/azure.tfstate"
+      sha256 "633033074a8ac43df3d0ef0881f14abd47a850b4afd5f1fbe02d3885b8e8104d"
+    end
+
     assert_match "v#{version}", shell_output("#{bin}/inframap version")
-    testpath.install resource("test_resource")
+    testpath.install resource("homebrew-test_resource")
     output = shell_output("#{bin}/inframap generate --tfstate #{testpath}/azure.tfstate")
     assert_match "strict digraph G {", output
     assert_match "\"azurerm_virtual_network.myterraformnetwork\"->\"azurerm_virtual_network.myterraformnetwork2\";",
